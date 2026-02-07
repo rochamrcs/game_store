c3d90887a2f45ed1c04857bc89dc0b24ed1f986e
rochamrcs <mavinicius95@gmail.com>
2026-02-07 15:00:54 -0300

    Adicionando validação de nome para verificar se um jogo ja existe

--- GameStore.Api/Endpoints/GamesEndpoints.cs
+++ GameStore.Api/Endpoints/GamesEndpoints.cs
@@ -8,11 +8,11 @@ public static class GamesEndPoints
 {
     const string GetGameEndpointName = "GetGame";
     private static readonly List<GameDto> games = [
-    new (1, "Pokemon Silver", "RPG", 79.99M, new DateOnly(1999, 11, 21)),
-    new (2, "The Legend of Zelda: Ocarina of Time", "Adventure", 99.99M, new DateOnly(1998, 11, 21)),
-    new (3, "Super Mario World", "Platform", 59.99M, new DateOnly(1990, 11, 21)),
-    new (4, "Final Fantasy VII", "RPG", 89.99M, new DateOnly(1997, 1, 31)),
-    new (5, "Metal Gear Solid", "Action", 69.99M, new DateOnly(1998, 9, 3)),
+        new (1, "Pokemon Silver", "RPG", 79.99M, new DateOnly(1999, 11, 21)),
+        new (2, "The Legend of Zelda: Ocarina of Time", "Adventure", 99.99M, new DateOnly(1998, 11, 21)),
+        new (3, "Super Mario World", "Platform", 59.99M, new DateOnly(1990, 11, 21)),
+        new (4, "Final Fantasy VII", "RPG", 89.99M, new DateOnly(1997, 1, 31)),
+        new (5, "Metal Gear Solid", "Action", 69.99M, new DateOnly(1998, 9, 3)),
     ];
 
     public static void MapGamesEndPoints(this WebApplication app)
@@ -30,6 +30,16 @@ public static class GamesEndPoints
         .WithName(GetGameEndpointName);
 
         group.MapPost("/", (CreateGameDto newGame) => {
+
+            var gameExists = games.Any(g =>
+                g.Name.Equals(newGame.Name, StringComparison.OrdinalIgnoreCase)
+            );
+
+            if (gameExists)
+            {
+                return Results.Conflict($"O jogo {newGame.Name} já existe.");
+            }
+
             GameDto game = new(
                 games.Count + 1,
                 newGame.Name,
@@ -37,6 +47,7 @@ public static class GamesEndPoints
                 newGame.Price,
                 newGame.ReleaseDate
             );
+
 
             games.Add(game);
 
