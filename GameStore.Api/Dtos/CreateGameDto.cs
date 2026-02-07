95eb0756e6180935a8eaa13013237aa6a74dffb7
rochamrcs <mavinicius95@gmail.com>
2026-02-07 15:00:05 -0300

    Adicionando validações nos endpoints POST e PUT

--- GameStore.Api/Dtos/CreateGameDto.cs
+++ GameStore.Api/Dtos/CreateGameDto.cs
@@ -1,9 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CreateGameStore.Api.Dtos;
 
 public record CreateGameDto(
     int id,
-    string Name,
-    string Genre,
-    decimal Price,
+    [Required][StringLength(50)] string Name,
+    [Required][StringLength(20)] string Genre,
+    [Required][Range(1,250)] decimal Price,
     DateOnly ReleaseDate
 );
