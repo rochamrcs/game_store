7138b6cb5ae20e387efcb5b54d10042c89c09c6c
rochamrcs <mavinicius95@gmail.com>
2026-02-08 07:55:25 -0300

    Implantando automação para popular a tabela de generos e refatorando o arquivo Program.cs

--- GameStore.Api/Data/DataExtensions.cs
+++ GameStore.Api/Data/DataExtensions.cs
@@ -1,3 +1,4 @@
+using GameStore.Api.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Api.Data;
@@ -11,4 +12,32 @@ public static class DataExtensions
 
         dbContext.Database.Migrate();
     }
+
+    public static void AddGameStoreDb(this WebApplicationBuilder builder)
+    {
+        var connString = "Data Source=GameStore.db";
+
+        builder.Services.AddSqlite<GameStoreContext>(
+            connString,
+            optionsAction: options => options.UseSeeding((context, _) =>
+            {
+                if (!context.Set<Genre>().Any())
+                {
+                    context.Set<Genre>().AddRange(
+                        new Genre {Name = "RPG"},
+                        new Genre {Name = "Adventure"},
+                        new Genre {Name = "Platformer"},
+                        new Genre {Name = "Racing"},
+                        new Genre {Name = "Sports"},
+                        new Genre {Name = "Fighting"},
+                        new Genre {Name = "Horror"},
+                        new Genre {Name = "Puzzle"}
+                    );
+
+                    context.SaveChanges();
+                }   
+            })
+            );
+    }
 }
+
