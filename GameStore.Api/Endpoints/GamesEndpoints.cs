08c6df60c7f907c0e3349100a894dcfd700ad742
rochamrcs <mavinicius95@gmail.com>
2026-02-09 19:48:02 -0300

    Ajustando o projeto, retirando a lista estatica de jogos

--- GameStore.Api/Endpoints/GamesEndpoints.cs
+++ GameStore.Api/Endpoints/GamesEndpoints.cs
@@ -10,13 +10,6 @@ namespace GameStore.Api.Endpoints;
 public static class GamesEndPoints
 {
     const string GetGameEndpointName = "GetGame";
-    private static readonly List<GameSummaryDto> games = [
-        new (1, "Pokemon Silver", "RPG", 79.99M, new DateOnly(1999, 11, 21)),
-        new (2, "The Legend of Zelda: Ocarina of Time", "Adventure", 99.99M, new DateOnly(1998, 11, 21)),
-        new (3, "Super Mario World", "Platform", 59.99M, new DateOnly(1990, 11, 21)),
-        new (4, "Final Fantasy VII", "RPG", 89.99M, new DateOnly(1997, 1, 31)),
-        new (5, "Metal Gear Solid", "Action", 69.99M, new DateOnly(1998, 9, 3)),
-    ];
 
     public static void MapGamesEndPoints(this WebApplication app)
     {
